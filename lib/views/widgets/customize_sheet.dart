fcafcf3d07fe811c9d94d8c5935630f7c9c68fe5
ahmed sayed <ahmedsaid50asr@gmail.com>
2025-11-20 10:43:36 +0200

    Add tap-to-unfocus behavior in bottom sheet using GestureDetector

--- lib/views/widgets/customize_sheet.dart
+++ lib/views/widgets/customize_sheet.dart
@@ -5,16 +5,20 @@ class CustomizeSheet extends StatelessWidget {
   const CustomizeSheet({super.key});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: const Column(
-          children: [
-            SizedBox(height: 32),
-            CustomTextfeild(hint: "title"),
-            SizedBox(height: 16),
-            CustomTextfeild(hint: "content", maxLines: 5),
-          ],
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: const Column(
+            children: [
+              SizedBox(height: 32),
+              CustomTextfeild(hint: "title"),
+              SizedBox(height: 16),
+              CustomTextfeild(hint: "content", maxLines: 5),
+            ],
+          ),
         ),
       ),
     );
